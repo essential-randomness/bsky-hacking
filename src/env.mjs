161f050d4fc5ab4a54defc326ea1bd0610cc5a66
Essential Randomness <essential.randomn3ss@gmail.com>
2023-05-03 23:20:00 -0700

    show followers to follow ratio

--- src/env.mjs
+++ src/env.mjs
@@ -1,5 +1,5 @@
-import { z } from "zod";
 import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
   /**
@@ -18,11 +18,13 @@ export const env = createEnv({
       // Since NextAuth.js automatically uses the VERCEL_URL if present.
       (str) => process.env.VERCEL_URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string().min(1) : z.string().url(),
+      process.env.VERCEL ? z.string().min(1) : z.string().url()
     ),
     // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
     DISCORD_CLIENT_ID: z.string(),
     DISCORD_CLIENT_SECRET: z.string(),
+    // NEXT_PUBLIC_BSKY_USERNAME: z.string(),
+    // NEXT_PUBLIC_BSKY_PASSWORD: z.string(),
   },
 
   /**
@@ -32,6 +34,8 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+    NEXT_PUBLIC_BSKY_USERNAME: z.string(),
+    NEXT_PUBLIC_BSKY_PASSWORD: z.string(),
   },
 
   /**
@@ -45,5 +49,7 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    NEXT_PUBLIC_BSKY_USERNAME: process.env.NEXT_PUBLIC_BSKY_USERNAME,
+    NEXT_PUBLIC_BSKY_PASSWORD: process.env.NEXT_PUBLIC_BSKY_PASSWORD,
   },
 });
